cf1b0bb7ba5c7e4d952d2fa32ecc4f0e3586f4ba
bruhnn <71610574+bruhnn@users.noreply.github.com>
2025-06-02 00:18:56 -0300

    ♻️ refactor: update main.py

--- main.py
+++ main.py
@@ -6,14 +6,21 @@ from PySide6.QtWidgets import QApplication
 
 from src.BD2ModManager import BD2ModManager
 from src.gui import MainWindow
+from src.gui.config import BD2MMConfigManager
 
 
 def main():
     app = QApplication(sys.argv)
+    
+    CURRENT_PATH = Path(__file__).parent
 
-    BD2MM = BD2ModManager(Path(__file__).parent / "mods")
+    BD2MMConfig = BD2MMConfigManager(CURRENT_PATH / "BD2ModManager.ini")
+    
+    print(BD2MMConfig.get("staging_mods_path"))
+    
+    BD2MM = BD2ModManager(BD2MMConfig.get("staging_mods_path"))
 
-    window = MainWindow(BD2MM)
+    window = MainWindow(BD2MM, BD2MMConfig)
     window.show()
 
     sys.exit(app.exec())
@@ -21,6 +28,6 @@ def main():
 
 if __name__ == "__main__":
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=logging.INFO,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     main()
