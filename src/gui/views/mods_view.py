caf7d9685c927212c0ffa5f7a35ec936f659b6fc
bruhnn <71610574+bruhnn@users.noreply.github.com>
2025-06-05 00:57:44 -0300

    ✨ feat: show visual indicator for conflicts

--- src/gui/views/mods_view.py
+++ src/gui/views/mods_view.py
@@ -1,4 +1,4 @@
-from PySide6.QtCore import Qt, Signal
+from PySide6.QtCore import Qt, Signal, QRect, QSize
 from PySide6.QtWidgets import (
     QWidget,
     QVBoxLayout,
@@ -18,7 +18,7 @@ from PySide6.QtWidgets import (
     QCheckBox,
     QGridLayout
 )
-from PySide6.QtGui import QDragEnterEvent, QIcon, QShortcut, QKeySequence, QColor, QBrush, QPalette
+from PySide6.QtGui import QDragEnterEvent, QIcon, QShortcut, QKeySequence, QColor, QBrush, QPalette, QPainter
 
 
 
@@ -46,13 +46,37 @@ class DragFilesModal(QWidget):
     
 class ModItemStyledDelegate(QStyledItemDelegate):
     def paint(self, painter, option, index):
+        super().paint(painter, option, index)
+        
         has_conflict = index.data(Qt.ItemDataRole.UserRole + 1)
-          
+        
         option.palette.setColor(QPalette.Text, QColor('#ecf0f1'))
+        
         if has_conflict:
             option.palette.setColor(QPalette.Text, QColor('#B4656F'))
             
-        super().paint(painter, option, index)
+            icon = QIcon(":/material/report.svg")
+            
+            icon_size = option.decorationSize
+            
+            if icon_size.width() == 0 or icon_size.height() == 0:
+                icon_size = QSize(16, 16)  
+
+            x = option.rect.right() - icon_size.width() - 8 
+            y = option.rect.top() + (option.rect.height() - icon_size.height()) // 2
+
+            icon.paint(painter, x, y, icon_size.width(), icon_size.height())
+
+
+        # super().paint(painter, option, index)
+
+    def sizeHint(self, option, index):
+        size = super().sizeHint(option, index)
+        icon_size = option.decorationSize
+        if icon_size.width() == 0:
+            icon_size.setWidth(16)
+        size.setWidth(size.width() + icon_size.width() + 8)  # espaço extra para o ícone
+        return size
         
         # painter.fillRect(option.rect, QColor("#B4656F"))
         
@@ -96,6 +120,7 @@ class ModsView(QWidget):
         self.search_field.setObjectName("searchField")
         self.search_field.addAction(QIcon(":/material/search.svg"), QLineEdit.ActionPosition.LeadingPosition)
         self.search_field.textChanged.connect(self._filter_search)
+        self.search_field.setFocusPolicy
         
         self.search_type = QComboBox()
         self.search_type.setObjectName("searchCombobox")
@@ -142,7 +167,8 @@ class ModsView(QWidget):
         self.mod_list.itemChanged.connect(self._mod_state_changed)
         # self.mod_list.itemSelectionChanged.connect(self._check_mod_conflicts)
         self.mod_list.customContextMenuRequested.connect(self.show_context_menu)
-        self.mod_list.setItemDelegate(ModItemStyledDelegate())
+        self.mod_list.setItemDelegateForColumn(0, ModItemStyledDelegate())
+        # self.mod_list.setItemDelegate(RightIconDelegate(QIcon(":/material/add.svg")))
         
         shortcut = QShortcut(QKeySequence("Ctrl+A"), self.mod_list)
         shortcut.activated.connect(self.mod_list.selectAll)
@@ -185,6 +211,8 @@ class ModsView(QWidget):
         layout.addWidget(self.top_bar)
         layout.addWidget(self.mod_list)
         layout.addWidget(self.footer_bar)
+        
+        self._check_all_mod_conflicts()
 
     def retranslateUI(self):
         self.setWindowTitle(self.tr("Mods Manager"))
@@ -242,8 +270,8 @@ class ModsView(QWidget):
             else:
                 menu.addAction("Enable Mod", lambda: self._enable_mods(selected_items))
     
-            menu.addAction(self.tr("Set Mod Author"), lambda: self._show_author_input_dialog(current_item))
-            menu.addAction(self.tr("Highlight conflicts"), self._highlight_mod_conflicts)
+            # menu.addAction(self.tr("Set Mod Author"), lambda: self._show_author_input_dialog(current_item))
+            # menu.addAction(self.tr("Highlight conflicts"), self._highlight_mod_conflicts)
             
             if self._has_conflict_highlights:
                 menu.addAction(self.tr("Remove highlights"), self._remove_hightlight_conflicts)
@@ -381,47 +409,71 @@ class ModsView(QWidget):
         mod_state = item.checkState(0)
         mod_data["enabled"] = mod_state == Qt.CheckState.Checked
         item.setData(0, Qt.ItemDataRole.UserRole, mod_data)
-        # self.modStateChanged.emit(
-        #     mod_data["name"], mod_state == Qt.CheckState.Checked
-        # )
+        self.modStateChanged.emit(
+            mod_data["name"], mod_state == Qt.CheckState.Checked
+        )
     
-    def _remove_hightlight_conflicts(self):
-        for index in range(self.mod_list.topLevelItemCount()):
-            item = self.mod_list.topLevelItem(index)
-            if item.data(0, Qt.ItemDataRole.UserRole + 1):
-                item.setData(0, Qt.ItemDataRole.UserRole + 1, False)
-        self._has_conflict_highlights = False
+    # def _remove_hightlight_conflicts(self):
+    #     for index in range(self.mod_list.topLevelItemCount()):
+    #         item = self.mod_list.topLevelItem(index)
+    #         if item.data(0, Qt.ItemDataRole.UserRole + 1):
+    #             item.setData(0, Qt.ItemDataRole.UserRole + 1, False)
+    #     self._has_conflict_highlights = False
     
-    def _highlight_mod_conflicts(self):
-        for index in range(self.mod_list.topLevelItemCount()):
-            item = self.mod_list.topLevelItem(index)
-            if item.data(0, Qt.ItemDataRole.UserRole + 1):
-                item.setData(0, Qt.ItemDataRole.UserRole + 1, False)
+    # def _highlight_mod_conflicts(self):
+    #     for index in range(self.mod_list.topLevelItemCount()):
+    #         item = self.mod_list.topLevelItem(index)
+    #         if item.data(0, Qt.ItemDataRole.UserRole + 1):
+    #             item.setData(0, Qt.ItemDataRole.UserRole + 1, False)
                 
-            # item.setBackground(0, QBrush())
-        if len(self.mod_list.selectedItems()) > 1:
-            return
+    #         # item.setBackground(0, QBrush())
+    #     if len(self.mod_list.selectedItems()) > 1:
+    #         return
         
-        selected_item = self.mod_list.currentItem()
-        selected_item_data = selected_item.data(0, Qt.ItemDataRole.UserRole)
+    #     selected_item = self.mod_list.currentItem()
+    #     selected_item_data = selected_item.data(0, Qt.ItemDataRole.UserRole)
                 
-        for index in range(self.mod_list.topLevelItemCount()):
-            item = self.mod_list.topLevelItem(index)
-            item_data = item.data(0, Qt.ItemDataRole.UserRole)
+    #     for index in range(self.mod_list.topLevelItemCount()):
+    #         item = self.mod_list.topLevelItem(index)
+    #         item_data = item.data(0, Qt.ItemDataRole.UserRole)
             
-            if item_data.get("character") is None:
-                continue
+    #         if item_data.get("character") is None:
+    #             continue
         
-            if not item_data["enabled"]:
-                continue
+    #         if not item_data["enabled"]:
+    #             continue
             
-            if selected_item_data["type"] == item_data["type"] and selected_item_data["character"]["id"] == item_data["character"]["id"]:
-                item.setData(0, Qt.ItemDataRole.UserRole + 1, True)
-                if not self._has_conflict_highlights:
-                    self._has_conflict_highlights = True
+    #         if selected_item_data["type"] == item_data["type"] and selected_item_data["character"]["id"] == item_data["character"]["id"]:
+    #             item.setData(0, Qt.ItemDataRole.UserRole + 1, True)
+    #             if not self._has_conflict_highlights:
+    #                 self._has_conflict_highlights = True
                 # for col in range(item.columnCount()):
                 #     item.setBackground(col, QColor('#B4656F'))
         # print(selected_item.data(0, Qt.ItemDataRole.UserRole))
+    
+    def _check_all_mod_conflicts(self):
+        for index in range(self.mod_list.topLevelItemCount()):
+            item = self.mod_list.topLevelItem(index)
+            item.setData(0, Qt.ItemDataRole.UserRole + 1, False)
+
+        mod_groups = {}
+        for index in range(self.mod_list.topLevelItemCount()):
+            item = self.mod_list.topLevelItem(index)
+            mod_data = item.data(0, Qt.ItemDataRole.UserRole)
+            if not mod_data.get("enabled"):
+                continue
+            char = mod_data.get("character")
+            if not char:
+                continue
+            key = (mod_data.get("type"), char.get("id"))
+            mod_groups.setdefault(key, []).append(item)
+
+        has_conflict = False
+        for items in mod_groups.values():
+            if len(items) > 1:
+                has_conflict = True
+                for item in items:
+                    item.setData(0, Qt.ItemDataRole.UserRole + 1, True)
 
     def load_mods(self, mods: list):
         pos_y = self.mod_list.verticalScrollBar().value()
@@ -463,3 +515,5 @@ class ModsView(QWidget):
         if not self._modlist_loaded:
             self._modlist_loaded = True
             self.mod_list.header().resizeSections(QHeaderView.ResizeMode.ResizeToContents)
+        
+        self._check_all_mod_conflicts()
