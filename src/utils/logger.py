3c620a12bacb02758268fbe30d67c1fe50d22dd1
bruhnn <71610574+bruhnn@users.noreply.github.com>
2025-06-29 01:44:12 -0300

    :bookmark_tabs: refactor: Enhance logger setup with level, filter, and formatting

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,23 +1,43 @@
 import logging
+from pathlib import Path
 
-def setup_logging(log_file_path):
+
+class LogFilter(logging.Filter):
+    def __init__(self, name: str) -> None:
+        self._name = name
+
+    def filter(self, record) -> bool:
+        if self._name == record.name:
+            return True
+        return False
+
+
+def setup_logging(
+    log_file: str | Path, level: str = "INFO", name_filter: str | None = None
+) -> None:
+    log_file = str(log_file)
+    level = level.upper()
+
     logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(level)
 
-    # Clear existing handlers
     logger.handlers.clear()
 
-    formatter = logging.Formatter('%(asctime)s - %(name)s @ %(levelname)s - %(funcName)s : %(message)s')
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s @ %(levelname)s - %(funcName)s:%(lineno)d : %(message)s"
+    )
 
-    # File handler
-    file_handler = logging.FileHandler(log_file_path, mode="w", encoding="UTF-8")
-    file_handler.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler(log_file, mode="w", encoding="UTF-8")
+    file_handler.setLevel(level)
     file_handler.setFormatter(formatter)
+    if name_filter is not None:
+        file_handler.addFilter(LogFilter(name_filter))
     logger.addHandler(file_handler)
 
-    # Console handler
     console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.DEBUG)
-    console_handler.setFormatter(formatter)  
-    
+    console_handler.setLevel(level)
+    if name_filter is not None:
+        console_handler.addFilter(LogFilter(name_filter))
+    console_handler.setFormatter(formatter)
+
     logger.addHandler(console_handler)
