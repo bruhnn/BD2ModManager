cd9d1665f889ce336359cebc0dc397e58dc20980
bruhnn <71610574+bruhnn@users.noreply.github.com>
2025-06-29 01:44:46 -0300

    :recycle: refactor: Refactor BD2Mod and related dataclasses for clarity and flexibility

--- src/utils/models.py
+++ src/utils/models.py
@@ -20,24 +20,25 @@ class BD2ModType(Enum):
 
     @property
     def display_name(self):
-        if self.value in (3,): # NPC
+        if self is BD2ModType.NPC:  # NPC
             return self.name.upper()
-        
+
         return self.name.capitalize()
-    
+
+
 @dataclass
 class BD2Mod:
     name: str
+    display_name: Optional[str]
     type: Optional[BD2ModType]
     path: str
-    relative_name: Optional[str]
     character_id: Optional[str]
     scene_id: Optional[str]
     npc_id: Optional[str]
     dating_id: Optional[str]
-    
+
     @classmethod
-    def from_mod_path(cls, path: Union[str, Path]):
+    def from_mod_path(cls, path: Union[str, Path], staging_path: Path):
         if not isinstance(path, Path):
             path = Path(path)
 
@@ -49,7 +50,10 @@ class BD2Mod:
         type_patterns = {
             BD2ModType.IDLE: re.compile(r"^char(\d+)\.modfile$", re.I),
             BD2ModType.CUTSCENE: re.compile(r"cutscene_char(\d+)\.modfile$", re.I),
-            BD2ModType.SCENE: re.compile(r"(?:specialillust|illust_special|storypack)(\d+)(?:_?\d+)?\.modfile$", re.I),
+            BD2ModType.SCENE: re.compile(
+                r"(?:specialillust|illust_special|storypack)(\d+)(?:_?\d+)?\.modfile$",
+                re.I,
+            ),
             BD2ModType.NPC: re.compile(r"^npc(\d+)\.modfile$", re.I),
             BD2ModType.DATING: re.compile(r"^illust_dating(\d+)\.modfile$", re.I),
         }
@@ -67,53 +71,74 @@ class BD2Mod:
         if mod_type is None:
             logger.warning("Unknown mod type for file: %s", path.name)
 
-        return cls(
-            type=mod_type,
-            name=path.name,
-            path=path.absolute().as_posix(),
-            relative_name = None,
-            character_id=mod_id if mod_type in (BD2ModType.IDLE, BD2ModType.CUTSCENE) else None,
-            scene_id=mod_id if mod_type == BD2ModType.SCENE else None,
-            npc_id=mod_id if mod_type == BD2ModType.NPC else None,
-            dating_id=mod_id if mod_type == BD2ModType.DATING else None,
-        )
+        kwargs = {
+            "type": mod_type,
+            "name": str(path.relative_to(staging_path).as_posix()),
+            "display_name": path.name,
+            "path": str(path.resolve()),
+            "character_id": None,
+            "scene_id": None,
+            "npc_id": None,
+            "dating_id": None,
+        }
+
+        if mod_id:
+            if mod_type in (BD2ModType.IDLE, BD2ModType.CUTSCENE):
+                kwargs["character_id"] = mod_id
+            elif mod_type == BD2ModType.SCENE:
+                kwargs["scene_id"] = mod_id
+            elif mod_type == BD2ModType.NPC:
+                kwargs["npc_id"] = mod_id
+            elif mod_type == BD2ModType.DATING:
+                kwargs["dating_id"] = mod_id
+
+        return cls(**kwargs)
+
 
 @dataclass
 class Character:
     id: str
     character: str
     costume: str
-    
+
     @classmethod
     def from_dict(cls, data: dict):
-        return cls(
-            id=data["id"],
-            character=data["character"],
-            costume=data["costume"]
-        )
-    
+        return cls(id=data["id"], character=data["character"], costume=data["costume"])
+
     def full_name(self, separator: Optional[str] = None):
-        return f"{self.character} {self.costume}" if not separator else f"{self.character} {separator} {self.costume}"
+        return (
+            f"{self.character} {self.costume}"
+            if not separator
+            else f"{self.character} {separator} {self.costume}"
+        )
+
 
 @dataclass
 class Scene:
     id: str
     name: str
 
+
 @dataclass
 class NPC:
     id: str
     name: str
 
+
 @dataclass
 class BD2ModEntry:
     mod: BD2Mod
-    enabled: bool
     author: Optional[str]
+    enabled: bool = False
     character: Optional[Character] = None
     scene: Optional[Scene] = None
     npc: Optional[NPC] = None
-    
+    has_conflict: bool = False
+
     @property
     def name(self):
         return self.mod.name
+
+    @property
+    def path(self):
+        return self.mod.path
